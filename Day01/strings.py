026813b2ce933131385292c1c14993a7ea893a1c
Abdullah Khan <abdullahjan2780@gmail.com>
2025-06-14 17:23:36 +0500

    day01

---
+++ Day01/strings.py
@@ -0,0 +1,3 @@
+print("abdullah" + " khan")
+
+input("What is your age? ")
---
+++ Day01/variables.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print("Hello " + name + "! You are " + age + " years old.")
+
+print(len(name))
+
+username = input("What is your username? ")
+length = len(username)
+print(length)
