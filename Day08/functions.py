cea663759c1dd6a310540d6b5c5b0840cf5ed278
Abdullah Khan <abdullahjan2780@gmail.com>
2025-08-17 15:37:41 +0500

    project_ciphertext

---
+++ Day08/functions.py
@@ -0,0 +1,13 @@
+def greet():
+    print("Hello, welcome to the program!")
+    print("nice to meet you!")
+    print("I hope you have a great day!")
+
+# greet()
+
+def greet_with_name(name):
+    print(f"Hello, {name} Welcome to the program!")
+    print(f"nice to meet you {name}!")
+    print(f"I hope you have a great day, {name}!")
+
+greet_with_name("Khan")
